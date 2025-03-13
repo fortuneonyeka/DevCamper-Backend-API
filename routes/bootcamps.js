ac87133a6328c436533e626fbcad4349f464ea21
fortune onyeka <ihedfortune@gmail.com>
2025-03-13 15:00:59 -0300

    create route controller and linked them to route using router

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,22 +1,19 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ success:true, msg:"Show all bootcamps"})
-})
+const router = express.Router();
 
-router.get("/:id", (req, res) => {
-  res.status(200).json({ success:true, msg:`Get a bootcamp ${req.params.id}`})
-})
-router.post("/", (req, res) => {
-  res.status(200).json({ success:true, msg:"Create new bootcamp"})
-})
-router.put("/:id", (req, res) => {
-  res.status(200).json({ success:true, msg:`Update a bootcamp ${req.params.id}`})
-})
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ success:true, msg:`Delete a bootcamp ${req.params.id}`})
-})
+router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
-
-module.exports = router
+module.exports = router;
