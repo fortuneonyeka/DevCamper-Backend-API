e6dd3bdc00206e063bcb0b28bb2818f3186735a6
fortune onyeka <ihedfortune@gmail.com>
2025-03-13 15:44:25 -0300

    custom loggin middleware

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const express  = require( "express" );
-const dotenv= require( "dotenv" );
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require('morgan')
 
 //Route files
-const bootcamps = require("./routes/bootcamps")
+const bootcamps = require("./routes/bootcamps");
 
 //Load env variable
-dotenv.config({path: "./config/config.env"})
+dotenv.config({ path: "./config/config.env" });
 
-const app = express()
+const app = express();
+
+//Dev logging middleware
+if(process.env.NODE_ENV === "development" ) {
+  app.use(morgan("dev"))
+}
 
 //Mount routers
-app.use("/api/v1/bootcamps", bootcamps)
-
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 6000;
 
-app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(
+    `server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
