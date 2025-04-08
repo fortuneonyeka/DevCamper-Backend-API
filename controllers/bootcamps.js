fbc0bc9df770a2d36a5d7fbdbb2e53ecb452bda1
fortune onyeka <ihedfortune@gmail.com>
2025-04-08 13:16:22 -0300

    add async handler middleware to handle trycatch and get rid of trycatch at every call

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,98 +1,79 @@
 const Bootcamp = require("../models/BootCamp");
 const mongoose = require("mongoose");
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
-    
-    res.status(200).json({
-      success: true,
-      count: bootcamps.length,
-      data: bootcamps
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
+});
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = async (req, res, next) => {
-  try {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return next(
-        new ErrorResponse(`Invalid ID format ${req.params.id}`, 400)
-      );
-    }
-
-    const bootcamp = await Bootcamp.findById(req.params.id);
-    
-    if (!bootcamp) {
-      return next(
-        new ErrorResponse(`Bootcamp not found with ID ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: bootcamp
-    });
-  } catch (err) {
-    next(err);
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(new ErrorResponse(`Invalid ID format ${req.params.id}`, 400));
   }
-};
+
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with ID ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body);
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: bootcamp
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = async (req, res, next) => {
-  try {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return next(
-        new ErrorResponse(`Invalid ID format ${req.params.id}`, 400)
-      );
-    }
-
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-
-    if (!bootcamp) {
-      return next(
-        new ErrorResponse(`Bootcamp not found with ID ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: bootcamp
-    });
-  } catch (err) {
-    next(err);
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(new ErrorResponse(`Invalid ID format ${req.params.id}`, 400));
   }
-};
+
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with ID ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
@@ -100,9 +81,7 @@ exports.updateBootcamp = async (req, res, next) => {
 exports.deleteBootcamp = async (req, res, next) => {
   try {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return next(
-        new ErrorResponse(`Invalid ID format ${req.params.id}`, 400)
-      );
+      return next(new ErrorResponse(`Invalid ID format ${req.params.id}`, 400));
     }
 
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
@@ -116,7 +95,7 @@ exports.deleteBootcamp = async (req, res, next) => {
     res.status(200).json({
       success: true,
       data: {},
-      message: `Bootcamp '${bootcamp.name}' was successfully deleted`
+      message: `Bootcamp '${bootcamp.name}' was successfully deleted`,
     });
   } catch (err) {
     next(err);
